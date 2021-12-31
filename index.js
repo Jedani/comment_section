5f6e66baa55150311b45b26b6aa181bcc54715a1
unknown <allansichangi35@gmail.com>
2022-01-01 02:37:24 +0300

    added first card

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function addImage() {
+	fetch("data.json")
+		.then((resp) => resp.json())
+		.then(
+			(data) =>
+				(document.querySelector("#ig").src = data.currentUser.image.png),
+		);
+}
+addImage();
 
+function one() {
+	fetch("data.json")
+		.then((resp) => resp.json())
+		.then((data) =>
+			console.log(data)(
+				(document.querySelector("#user").src = data.comments[0].user.image.png),
+				(document.querySelector("#name").innerText =
+					data.comments[0].user.username),
+				(document.querySelector("#dits").innerText =
+					data.comments[0].createdAt),
+				(document.querySelector(".coments").innerText =
+					data.comments[0].content),
+			),
+		);
+}
+one();
