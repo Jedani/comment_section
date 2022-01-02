0b6fa1509670653aca563b24968d0c4b1c784c31
unknown <allansichangi35@gmail.com>
2022-01-02 19:09:03 +0300

    tinkered with js

--- index.js
+++ index.js
@@ -1,31 +1,53 @@
-function addImage() {
-	fetch("data.json")
-		.then((resp) => resp.json())
-		.then((data) =>
-			(document.querySelector("#res").src = data.currentUser.image.png)(
-				(document.querySelector("#ig").src = data.currentUser.image.png),
-			),
-		);
-}
-addImage();
+fetch("data.json")
+	.then((resp) => resp.json())
+	.then((data) => main(data));
 
-function one() {
+function main(data) {
+	const comments = data.comments;
+	console.log(comments);
+	console.log(data);
+
+	function checker(currentUser, user, id) {}
+
+	comments.forEach((element) => {
+		checker(data.currentUser.username, element.user.username, element.id);
+	});
+
 	fetch("data.json")
 		.then((resp) => resp.json())
-		.then((data) =>
-			console.log(data)(
+		.then((data) => {
+			let cont = document.querySelector(".first");
+			if (cont.className == "first") {
 				(document.querySelector("#user").src = data.comments[0].user.image.png),
-				(document.querySelector("#name").innerText =
-					data.comments[0].user.username),
-				(document.querySelector("#dits").innerText =
-					data.comments[0].createdAt),
-				(document.querySelector(".coments").innerText =
-					data.comments[0].content),
-				(document.querySelector(".count").innerText = data.comments[0].score),
-			),
-		);
+					(document.querySelector("#name").innerText =
+						data.comments[0].user.username),
+					(document.querySelector("#dits").innerText =
+						data.comments[0].createdAt),
+					(document.querySelector(".coments").innerText =
+						data.comments[0].content),
+					(document.querySelector(".count").innerText = data.comments[0].score);
+			}
+			let sec = document.querySelector(".second");
+			if (sec.className == "second") {
+				(document.querySelector("#user").src = data.comments[1].user.image.png),
+					(document.querySelector("#name").innerText =
+						data.comments[1].user.username),
+					(document.querySelector("#dits").innerText =
+						data.comments[1].createdAt),
+					(document.querySelector(".coments").innerText =
+						data.comments[1].content),
+					(document.querySelector(".count").innerText = data.comments[1].score);
+			}
+		});
 }
-one();
+
+// function two() {
+// 	fetch("data.json")
+// 		.then((resp) => resp.json())
+// 		.then((data) => {
+
+// }
+// two();
 
 const content = document.querySelector(".open");
 const reply = document.querySelector("#reply");
@@ -36,4 +58,14 @@ reply.onclick = function () {
 	} else {
 		content.className = "open";
 	}
+};
+
+const up = document.querySelector("#max");
+const down = document.querySelector("#min");
+let val = document.querySelector(".count").innerText;
+up.onclick = function () {
+	console.log(val);
+};
+down.onclick = function () {
+	alert("me");
 };
